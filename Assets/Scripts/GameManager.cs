8a14eb366c6872f52979af79afb111cdca385bfc
Unknown <juwinviray@gmail.com>
2018-01-31 18:19:04 -0800

    Added Sprites, Starting Game Logic

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static int width = 4; //will be doubled for symmetry
+    public GameObject[] gameBoard = new GameObject[width * 2];
+    public GameObject playerOne;
+    public GameObject playerTwo;
+    public int movementMultiplier = 2;
+    public int playerOneIndex = width - 1;
+    public int playerTwoIndex = width;
+    private void Awake()
+    {
+        gameBoard[playerOneIndex] = playerOne;
+        gameBoard[playerTwoIndex] = playerTwo;
+    }
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //move left
+		if(Input.GetKeyDown("z"))
+        {
+            gameBoard[playerOneIndex--];
+            gameBoard[playerTwoIndex--];
+        }
+	}
+}
